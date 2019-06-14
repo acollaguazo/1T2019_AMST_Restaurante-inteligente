061c2f8df2504ae1932c2aa347a534beaad22b1f
Josue Sanchez <josssanc@espol.edu.ec>
2019-06-13 22:24:06 -0500

    Tareas de desarrollo movil realizadas, aun falta mejorar la ejecucion global

---
+++ aplicacion/app/src/main/java/com/example/grupo4/disponibilidad.java
@@ -0,0 +1,14 @@
+package com.example.grupo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class disponibilidad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disponibilidad);
+    }
+}
